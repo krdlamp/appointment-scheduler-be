4db5e9e8ec39f5a7774adf7afdf0807797dc5344
krdlamp <maylynstalampas@gmail.com>
2016-05-04 14:25:47 +0800

    Add subject to appointments

---
+++ database/migrations/2016_05_04_062417_add_subject_column_to_appointments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSubjectColumnToAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->string('subject');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            //
+        });
+    }
+}
